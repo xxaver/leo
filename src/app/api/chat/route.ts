5d38d4b9036ca504180104bdf1c633eceaa2f86b
xxaver <114171465+xxaver@users.noreply.github.com>
2025-07-19 18:12:27 +0200

    genau

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,46 +1,13 @@
 import {google} from '@ai-sdk/google';
-import {createDataStreamResponse, streamObject, streamText} from 'ai';
+import {createDataStreamResponse, streamObject} from 'ai';
 import {systemPrompt} from "@/data/systemPrompt";
-import {z} from "zod";
 import {getSchema} from "@/data/schema";
 import {v4} from "uuid";
-
-const tools = {
-    displayDetailsForID: {
-        type: "function",
-        parameters: z.object({
-            id: z.string().describe("The ID of the object to display"),
-        }),
-        description: "Displays details for the object with the given ID",
-    },
-    displayNextPromptSuggestions: {
-        type: "function",
-        description: "Displays suggestions for what the user might want to ask next",
-        parameters: z.object({
-            suggestions: z.array(z.object({
-                short: z.string().describe("The text that will be shown on the button for the user to press."),
-                full: z.string().describe("The prompt that will be sent if the user chooses this suggestion."),
-            })).describe("The suggestions to display")
-        }),
-    }
-}
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
     const {messages} = await req.json();
-    // console.log(systemPrompt)
-
-    //return streamText({
-    //    model: google('gemini-2.5-flash'),
-    //    system: systemPrompt,
-    //    messages,
-    //}).toDataStreamResponse({
-    //    getErrorMessage: (error) => {
-    //        console.log(error)
-    //        return "An error occurred."
-    //    }
-    //});
 
     return createDataStreamResponse({
         execute: async dataStream => {
@@ -51,18 +18,11 @@ export async function POST(req: Request) {
                     schema: getSchema(),
                     messages,
                 });
-                console.log("RESULT", result)
-
-                // dataStream.write(`f:{${JSON.stringify({
-                //     messageId: (await result.response).id
-                // })}}\n`);
                 let i = 0;
                 for await (const str of result.textStream) {
                     console.log(str)
-                    if(i === 0) {
-                        const id = v4(); 
-                        // const id = (await result.response).id;
-                        console.log(id)
+                    if (i === 0) {
+                        const id = v4();
                         dataStream.write(`f:${JSON.stringify({
                             messageId: id
                         })}\n`);
@@ -78,8 +38,7 @@ export async function POST(req: Request) {
                 dataStream.write(`e:${JSON.stringify(json)}\n`);
                 delete json.isContinued;
                 dataStream.write(`d:${JSON.stringify(json)}\n`);
-            }
-            catch (e) {
+            } catch (e) {
                 console.error(e)
             }
         }
