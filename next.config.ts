97eaf6fead2fe1e12abe5be107dd1ac3f1801762
xxaver <114171465+xxaver@users.noreply.github.com>
2025-07-20 10:23:38 +0200

    Add ESLint setting, refine schemas, improve event and entity handling

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     devIndicators: false,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     eslint: {
         ignoreDuringBuilds: true,
     }
