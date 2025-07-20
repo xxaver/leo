2d78be387fd7866fec826edf23425d5f1614388b
xxaver <114171465+xxaver@users.noreply.github.com>
2025-07-20 18:35:48 +0200

    Filter out invalid news entries in `news.ts` and adjust `news.json` by nullifying unsupported news items.

--- src/data/news.ts
+++ src/data/news.ts
@@ -11,4 +11,4 @@ export const lesescouts_17_7_25: News = {
     Wer sich also noch mit Lesestoff für die Ferien eindecken möchte, sollte unbedingt in der Schülerbücherei vorbeischauen. Die Auswahl an neuen Büchern ist groß, eure Ferien sind gerettet!
     Vielen Dank auch an Frau Rapp für ihr Vertrauen in die Lesescouts.`
 }
-export const news = Object.values(actualNews) as News[];
+export const news = Object.values(actualNews).filter(Boolean) as News[];
