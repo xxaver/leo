7652e62ebe1a03d3a3ebdc7d5f740a1a479338fc
xxaver <114171465+xxaver@users.noreply.github.com>
2025-07-22 15:01:09 +0200

    Refine prompt suggestion logic in `PromptSuggestion.tsx` by introducing `ready` state for better readability and improving button styling and hover behavior.

--- src/app/PromptSuggestion.tsx
+++ src/app/PromptSuggestion.tsx
@@ -5,10 +5,11 @@ export const PromptSuggestion: FC<PropsWithChildren<{ prompt: string; submit?: b
     ({prompt, children}) => {
         const submit = true;
         const {setInput, inputRef, append, status} = useContext(ChatContext)!;
+        const ready = status === "ready" || status === "error";
 
         return <button
             onClick={() => {
-                if(status !== "ready") return;
+                if(!ready) return;
                 if (submit) {
                     append({
                         role: "user",
@@ -19,7 +20,7 @@ export const PromptSuggestion: FC<PropsWithChildren<{ prompt: string; submit?: b
                     setInput(prompt);
                 }
             }}
-            className={"hover:bg-accent whitespace-nowrap line-clamp-1 overflow-ellipsis [&>svg]:stroke-red-600 [&>svg]:shrink-0 flex items-center gap-2 text-gray-700 font-semibold bg-white p-2 px-3 rounded-lg border border-red-600 hover:border-red-500 transition-colors text-left " + (status !== "ready" ? "opacity-70" : "cursor-pointer")}>
+            className={"whitespace-nowrap line-clamp-1 overflow-ellipsis [&>svg]:stroke-red-600 [&>svg]:shrink-0 flex items-center gap-2 text-gray-700 font-semibold bg-white p-2 px-3 rounded-lg border border-red-600 hover:border-red-500 transition-colors text-left " + (!ready ? "opacity-70" : "cursor-pointer hover:bg-accent")}>
             {children}
         </button>
     }
