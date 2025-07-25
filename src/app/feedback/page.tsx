0f72b08eefa72ef0bd0ee60d471aa2d66a1df681
xxaver <114171465+xxaver@users.noreply.github.com>
2025-07-25 09:41:42 +0200

    feedback

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -4,7 +4,7 @@ import {LanguagePicker} from "@/LanguagePicker";
 
 export default function Feedback() {
     return <Header items={<LanguagePicker />}>
-        <div className="min-h-0 grows overflow-auto bg-gray-100 flex justify-center p-3">
+        <div className="min-h-0 grow overflow-auto bg-gray-100 flex justify-center p-3">
             <div className="w-full max-w-md">
                 <FeedbackForm />
             </div>
