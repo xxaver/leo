b4e2c3011dc0f95a303e819dc26449a4c4b1c98d
xxaver <114171465+xxaver@users.noreply.github.com>
2025-09-29 18:03:55 +0200

    Comment out `scrapeNews` invocation in `scraper/index.ts` and clear unused `news.json` data.

--- scraper/index.ts
+++ scraper/index.ts
@@ -1,9 +1,8 @@
 import {scrapeOther} from "./other";
 import {target} from "./config";
-import { scrapeNews } from "./news";
 
 export const scrape = async () => {
-    await scrapeNews(target);
+    // await scrapeNews(target);
     await scrapeOther(target);
 };
 scrape();
